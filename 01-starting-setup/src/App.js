13e10f551f7f8c62b85af94f7b9023379f4b9db6
ppatel2022 <ppatel@marinerfinance.com>
2021-08-15 12:10:43 -0400

    2

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,7 +1,58 @@
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2021, 2, 28),
+    },
+    { id: "e2", 
+      title: "Rice", 
+      amount: 23.32, 
+      date: new Date(2021, 3, 19), 
+    },
+    {
+      id: "e3",
+      title: "Electic bill",
+      amount: 134.76,
+      date: new Date(2021, 5, 8),
+    },
+    {
+      id: "e4",
+      title: "Frying Oil",
+      amount: 56.43,
+      date: new Date(2021, 1, 6),
+    },
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
+      <p> This is also Visible.</p>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+     
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
+    
     </div>
   );
 }
---
+++ 01-starting-setup/src/components/ExpenseDate.js
@@ -0,0 +1,16 @@
+import './ExpenseDate.css';
+function ExpenseDate(props) {
+    const Month = props.date.toLocaleString("en-us", { month: "long" });
+    const Day = props.date.toLocaleString("en-us", { day: "2-digit" });
+    const Year = props.date.getFullYear();
+
+return (
+  <div className='expense-date'>
+    <div className='expense-date__month'>{Month}</div>
+    <div className='expense-date__day'>{Day}</div>
+    <div className='expense-date__year'>{Year}</div>
+  </div>
+);
+}
+
+export default ExpenseDate;
