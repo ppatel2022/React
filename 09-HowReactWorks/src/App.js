3cc09d221b827f550280933b9f5322ed7e1980f3
ppatel2022 <ppatel@marinerfinance.com>
2021-12-16 16:58:47 -0500

    commit-12-16-2021

--- 09-HowReactWorks/src/App.js
+++ 09-HowReactWorks/src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
-
+import React, { useState, useCallback } from 'react';
+import Button from './components/UI/Button/Button'
 import './App.css';
-
+import DemoOutput from './components/Demo/DemoOutput';
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggele, setAllowToggele] = useState(false);
+  console.log(' APP RUNNING...')
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggele) {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph); 
+    }
+  }, [allowToggele]);
+
+  const allowTogglerHandler = () => {
+    setAllowToggele(true);
+  }
   return (
-    <div className="app">
-      <h1>Hi there!</h1>
+    <div>
+      <div className="app">
+        <h1>Hi there!</h1>
+        <Button onClick={allowTogglerHandler}>Allow Toggeling.</Button>
+        <Button onClick={toggleParagraphHandler}>Toggle Paragraph.</Button>
+      </div>
+      <div className="app">
+        <DemoOutput show={showParagraph} />
+      </div>
     </div>
   );
 }
