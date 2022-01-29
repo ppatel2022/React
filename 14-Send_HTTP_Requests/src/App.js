e95eaedc7e6d19eab04649c1eb29fb5a68de93f7
ppatel2022 <ppatel@marinerfinance.com>
2022-01-29 13:40:53 -0500

    Send POST Request

--- 14-Send_HTTP_Requests/src/App.js
+++ 14-Send_HTTP_Requests/src/App.js
@@ -1,32 +1,58 @@
-import React from 'react';
-
+import React, {useState, useEffect, useCallback} from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
-  const dummyMovies = [
-    {
-      id: 1,
-      title: 'Some Dummy Movie',
-      openingText: 'This is the opening text of the movie',
-      releaseDate: '2021-05-18',
-    },
-    {
-      id: 2,
-      title: 'Some Dummy Movie 2',
-      openingText: 'This is the second opening text of the movie',
-      releaseDate: '2021-05-19',
-    },
-  ];
+const [movies, setMovies] = useState([]);
+const [isLoading, setIsLoading] = useState(false);
+const [error, setError] = useState(false);
+  const  fetchMoviesHandler = useCallback(async ()  => {
+    setIsLoading(true);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/');
+      if (response.status !== 200) {
+        throw new Error(' Serve Error....')
+      }
+      const data = await response.json(); // Built-in Method in Reponse to Translate JOSN response in JavaScript Body.
+      const transformedMovies = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+          
+        };
+      }); // End of transformedMovies
+      setMovies(transformedMovies);
+    } catch (error) {
+        setError(error.message);
+    }
+  setIsLoading(false);
+  }, []);
+  
+useEffect( () => {    // Load List when browser Render Page...
+  fetchMoviesHandler();
+}, [fetchMoviesHandler]);
 
+  let content = <p>Found no movies.</p>;
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />
+  }
+  if (movies.length === 0 && !error) {
+    content = <p>Found No Movies</p>
+  }
+  if (error) {
+    content = <p>{error}</p>
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>
+  }
   return (
     <React.Fragment>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        <MoviesList movies={dummyMovies} />
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
